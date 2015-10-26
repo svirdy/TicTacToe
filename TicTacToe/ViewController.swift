8ed4089034266ca664344d456187ad2f169cc6bf
Sachin Virdy <svirdy1@gmail.com>
2015-10-26 15:55:47 -0700

    set up board

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var button: UIButton!
+    
+    @IBAction func buttonPressed(sender: AnyObject) {
+        var image = UIImage(named: "cross.png")
+        sender.setImage(image, forState: .Normal)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
